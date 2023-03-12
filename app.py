64efcce0e400263e8ab589e677cf081070445e58
Zera Holladay <zeraholladay@gmail.com>
2023-03-12 18:41:28 -0400

    serialization improved and started column name filter.

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
-from flask import Flask, jsonify
-from flask import request
-
-from sqlalchemy import create_engine
+from flask import Flask, jsonify, request
+from sqlalchemy import MetaData, create_engine
+from sqlalchemy.orm import Session
 
 from reflect import Reflect
 
 url = "postgresql+psycopg2://postgres:mysecretpassword@localhost:5432/postgres"
+schema = "public"
 
 engine = create_engine(url)
-reflection = Reflect(engine, schema="public")
+default_reflection = Reflect(engine, schema="public")
 
 app = Flask(__name__)
 
 
 @app.route("/tables", methods=["GET"])
 def list_tables():
-    result = reflection.list_tables()
+    result = default_reflection.list_tables()
     return jsonify(result.dict())
 
 
 @app.route("/tables/<string:table_name>", methods=["GET"])
 def table_paginator(table_name):
-    print(request.path)
     page_number = request.args.get("page_number", 0, int)
     per_page = request.args.get("per_page", 100, int)
-    result = reflection.paginate(table_name, page_number, per_page)
+    # TODO
+    # column_names = request.args.getlist("column_names")
+    result = default_reflection.paginate(
+        table_name, page_number, per_page, column_names=[]
+    )
     return jsonify(result.dict())
--- reflect.py
+++ reflect.py
@@ -1,15 +1,9 @@
-from sqlalchemy import MetaData
-from sqlalchemy import select
+from datetime import date, datetime
+from typing import Dict, List
+
+from pydantic import BaseModel, parse_obj_as
+from sqlalchemy import Engine, MetaData, select
 from sqlalchemy.orm import Session
-from pydantic import BaseModel
-
-from sqlalchemy import create_engine
-
-
-class Engine(object):
-    @staticmethod
-    def create_engine(url):
-        return create_engine(url)
 
 
 class Tables(BaseModel):
@@ -19,34 +13,68 @@ class Tables(BaseModel):
 
 class PageOfResults(BaseModel):
     count: int
-    rows: list[dict]
+    results: list[dict]
     next_url: str
 
-    @staticmethod
-    def format_next_url(page_number: int, per_page: int) -> str:
-        page_number += 1
-        return f"?page_number={page_number}&per_page={per_page}"
+
+def _format_next_url(page_number: int, per_page: int) -> str:
+    page_number += 1
+    return f"?page_number={page_number}&per_page={per_page}"
 
 
 class Reflect(object):
-    def __init__(self, engine: any, schema: any):
-        self.metadata = MetaData(schema=schema)
+    model = Dict
+
+    def __init__(self, engine: Engine, **kwargs):
+        self.metadata = MetaData(**kwargs)
         self.metadata.reflect(bind=engine)
-        self.session = Session(engine, future=True)
+        self.engine = engine
 
     def list_tables(self) -> Tables:
         tables = [table for table in self.metadata.tables]
         return Tables(count=len(tables), tables=tables)
 
+    def format_object(self, obj) -> any:
+        if isinstance(obj, (datetime, date)):
+            return obj.isoformat()  # ISO 8601
+        else:
+            return obj
+
+    def to_dict(self, row, column_names=[]) -> dict:
+        return {name: self.format_object(getattr(row, name)) for name in column_names}
+
+    def parse_obj_as(self, db_results, column_names=[]):
+        return parse_obj_as(
+            List[self.model],
+            [self.to_dict(row, column_names=column_names) for row in db_results],
+        )
+
+    def build_select(self, table: str, column_names=[]):
+        return select(table)
+
     def paginate(
-        self, table_name: str, page_number: int, per_page: int
+        self, table_name: str, page_number=0, per_page=100, column_names=[]
     ) -> PageOfResults:
         table = self.metadata.tables[table_name]
-        statement = select(table).offset(page_number * per_page).limit(per_page)
-        results = self.session.execute(statement).all()
-        rows = [dict(row._mapping) for row in results]
+
+        column_names = (
+            [column.name for column in table.columns]
+            if not column_names
+            else column_names
+        )
+
+        with Session(self.engine) as session:
+            db_results = session.execute(
+                self.build_select(table, column_names=[])
+                .offset(page_number * per_page)
+                .limit(per_page)
+            ).all()
+
+        results = self.parse_obj_as(db_results, column_names=column_names)
+
+        # FIXME net_url should probably be null if there are no additional pages
         return PageOfResults(
-            count=len(rows),
-            rows=rows,
-            next_url=PageOfResults.format_next_url(page_number, per_page),
+            count=len(results),
+            results=results,
+            next_url=_format_next_url(page_number, per_page),
         )
